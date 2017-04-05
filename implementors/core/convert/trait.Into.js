b8ac5c3a38bfba6a036b28b871628841ffe5d620
Travis CI User <travis@example.org>
2017-04-05 14:04:51 +0000

    Documentation for nix-rust/nix@44c0ecb

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["libc"] = [];
+implementors["nix"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;&amp;'a mut <a class='struct' href='nix/sys/epoll/struct.EpollEvent.html' title='nix::sys::epoll::EpollEvent'>EpollEvent</a>&gt; for <a class='enum' href='https://doc.rust-lang.org/nightly/core/option/enum.Option.html' title='core::option::Option'>Option</a>&lt;&amp;'a mut <a class='struct' href='nix/sys/epoll/struct.EpollEvent.html' title='nix::sys::epoll::EpollEvent'>EpollEvent</a>&gt;",];
+implementors["void"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
